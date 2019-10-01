7eff9556190fb927e488cec7a76728cd042459d5
Caoimhe Malone <caoimhemalone@Caoimhes-MacBook-Air.local>
2019-10-01 20:32:54 +0100

    first commit

---
+++ hello-react/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+//includes
+import './Assets/css/default.min.css';
+
+//components
+import Dash from './components/mainComponents/dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dash />
+    </div>
+  );
+}
+
+export default App;
---
+++ hello-react/src/components/mainComponents/dashboard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function Dash() {
+  return (
+    <div className="App">
+      <section className="date">date</section>
+      <section className="weather"> weather </section>
+      <div className="mainInfo">
+	      <section className="time">time </section>
+	      <section className="hello-caoimhe"> Hello, Caoimhe </section>
+	      <section className="quote"> quote </section>
+      </div>
+    </div>
+  );
+}
+
+export default Dash;
